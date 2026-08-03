4bdcc3ab8d7d4cb5b2aa95fd27525ad19abff26c
walawala254 <dbmbryson@gmail.com>
2026-08-03 10:11:54 +0300

    fix: remove edge-scroll page chaining

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 import { initNavigation } from "./src/scripts/navigation.js";
-import { initPageFlow } from "./src/scripts/page-flow.js";
 import { initReveals } from "./src/scripts/reveal.js";
 
 document.documentElement.classList.add("js");
 
 initNavigation();
 initReveals();
-
-if (document.body.dataset.pageFlow !== "disabled") {
-  initPageFlow();
-}
